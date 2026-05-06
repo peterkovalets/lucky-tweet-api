ed48cb1877ba9d73aff76e2bc983041d8a07d898
Peter Kovalets <peterkovalets@proton.me>
2026-05-06 19:58:34 +0700

    Add format and length constraints to RegisterRequest

--- src/main/java/org/peterkovalets/lucky_tweet_api/dto/RegisterRequest.java
+++ src/main/java/org/peterkovalets/lucky_tweet_api/dto/RegisterRequest.java
@@ -2,11 +2,14 @@ package org.peterkovalets.lucky_tweet_api.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-    @NotBlank String username,
-    @Email String email,
-    @Size(min = 8) @NotBlank String password
+    @Pattern(regexp = "^[a-zA-Z0-9._-]+$",
+            message = "can only contain Latin letters, digits, hyphens, and dots")
+    @NotBlank @Size(min = 6, max = 30) String username,
+    @NotBlank @Email String email,
+    @NotBlank @Size(min = 8, max = 128) String password
 ) {
 }
